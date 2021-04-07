afcaa8b27c372c9b142929f76cf569e9c5f18e8c
dagreatcode <dagreatcode@gmail.com>
2021-04-07 02:27:02 -0400

    app starter

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
-
-// const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("client/build"));
 
-mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/bitbybit", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log("Successfully connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB", err);
-  });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bitbybit", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected.");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
 
 app.get("/api/config", (req, res) => {
   res.json({
@@ -32,12 +34,10 @@ app.get("/api/config", (req, res) => {
   });
 });
 
-app.use(routes);
-
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`App is running on http://localhost:${PORT}`);
 });
